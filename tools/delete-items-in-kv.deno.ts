d963be2c0913b67b7885817b448dfa06d4f8d675
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-07-26 03:28:10 +0900

    kvのデータ削除スクリプトが何もしていなかったのを修正

--- tools/delete-items-in-kv.deno.ts
+++ tools/delete-items-in-kv.deno.ts
@@ -1,5 +1,9 @@
 const kv = await Deno.openKv();
 const iterable = kv.list({ prefix: [] });
+let cnt = 0;
 for await (const item of iterable) {
-  console.log(item.key);
+  kv.delete(item.key);
+  console.log("deleted", item.key);
+  cnt++;
 }
+console.log(`deleted ${cnt} items`);
