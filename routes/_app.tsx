5ed00bb2012b6d68283c84f3f6af99732c67b62c
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-07-25 20:04:46 +0900

    ページの一部スタイルを調整

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,14 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html class="w-[100vw] h-[100vh]">
+    <html class="w-[100vw] h-[100svh]">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>七五 | Shichigo - 七語のリズムでSNS</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body class="w-[100vw] h-[100vh] bg-checker text-v-rl flex flex-col items-center overflow-hidden">
+      <body class="w-[100vw] h-[100svh] bg-checker text-v-rl flex flex-col items-center overflow-hidden">
         <Component />
       </body>
     </html>
