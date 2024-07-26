7a91be5bd7120ca82620b26df0fca2c201b10b74
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-07-26 12:48:46 +0900

    エンプティステートを追加

--- islands/PoemKnob.tsx
+++ islands/PoemKnob.tsx
@@ -55,7 +55,25 @@ export default function PoemKnob() {
         ref={dialogRef}
         class="dialog-base"
       >
-        <h2 class="text-xl font-bold">詩を詠む</h2>
+        <div class="flex items-center justify-between">
+          <h2 class="text-xl font-bold">詩を詠む</h2>
+          <input
+            type="file"
+            accept="image/*"
+            class="sg-button gray w-12 h-12 rounded-full self-end grid place-content-center"
+            onClick={onClickClosePoemDrawer}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              width="16"
+              height="16"
+            >
+              <path d="M16 13.25A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25V2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75ZM1.75 2.5a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h.94l.03-.03 6.077-6.078a1.75 1.75 0 0 1 2.412-.06L14.5 10.31V2.75a.25.25 0 0 0-.25-.25Zm12.5 11a.25.25 0 0 0 .25-.25v-.917l-4.298-3.889a.25.25 0 0 0-.344.009L4.81 13.5ZM7 6a2 2 0 1 1-3.999.001A2 2 0 0 1 7 6ZM5.5 6a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0Z">
+              </path>
+            </svg>
+          </input>
+        </div>
         <input
           class="border-[--color-border] border-2 p-8 rounded-2xl"
           placeholder="一句詠む ここタップして 気のままに"
--- routes/index.tsx
+++ routes/index.tsx
@@ -29,7 +29,13 @@ export default function Home(props: PageProps<Poem[]>) {
         <HaigoPlate />
       </header>
       <main class="flex flex-col items-center w-[calc(100vw_-_64px)] h-[min(36em, 80%)] border-[--color-border] pr-4 py-8 bg-[var(--color-white)] overflow-x-scroll snap-mandatory snap-x">
-        {props.data.map((item) => <Card item={item} />)}
+        {props.data.length > 0
+          ? props.data.map((item) => <Card item={item} />)
+          : (
+            <div class="w-[calc(100vw_-_128px)] h-[32em] text-[var(--color-gray)] font-bold grid place-content-center">
+              まだ詠まれた詩がないよ
+            </div>
+          )}
       </main>
     </Fragment>
   );
