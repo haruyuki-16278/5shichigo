76e9b6b2c79b8ddf7d7c3ea8b9cd296d5f8d009c
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-07-27 15:53:48 +0900

    svgをSpritesにまとめるようにした

--- islands/PoemKnob.tsx
+++ islands/PoemKnob.tsx
@@ -6,6 +6,7 @@ export default function PoemKnob() {
   const isOpenPoemDrawer = useSignal(false);
   const dialogRef = useRef<HTMLDialogElement>(null);
   const poemInputRef = useRef<HTMLInputElement>(null);
+  const poemImageInputRef = useRef<HTMLInputElement>(null);
   const [errorMessage, setErrorMessage] = useState("");
 
   isOpenPoemDrawer.subscribe((value) => {
@@ -57,22 +58,23 @@ export default function PoemKnob() {
       >
         <div class="flex items-center justify-between">
           <h2 class="text-xl font-bold">詩を詠む</h2>
-          <input
-            type="file"
-            accept="image/*"
+          <button
             class="sg-button gray w-12 h-12 rounded-full self-end grid place-content-center"
-            onClick={onClickClosePoemDrawer}
+            onClick={() => poemImageInputRef.current?.click()}
           >
+            <input
+              type="file"
+              accept="image/*"
+              class="hidden"
+              ref={poemImageInputRef}
+            />
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
               width="16"
               height="16"
             >
-              <path d="M16 13.25A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25V2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75ZM1.75 2.5a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h.94l.03-.03 6.077-6.078a1.75 1.75 0 0 1 2.412-.06L14.5 10.31V2.75a.25.25 0 0 0-.25-.25Zm12.5 11a.25.25 0 0 0 .25-.25v-.917l-4.298-3.889a.25.25 0 0 0-.344.009L4.81 13.5ZM7 6a2 2 0 1 1-3.999.001A2 2 0 0 1 7 6ZM5.5 6a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0Z">
-              </path>
+              <use xlink:href="#icon-image" />
             </svg>
-          </input>
+          </button>
         </div>
         <input
           class="border-[--color-border] border-2 p-8 rounded-2xl"
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,7 @@
 import { type PageProps } from "$fresh/server.ts";
 import { Header } from "../components/Header.tsx";
+import { Sprites } from "../components/Sprites.tsx";
+
 export default function App({ Component }: PageProps) {
   return (
     <html class="w-[100vw] h-[100svh]">
@@ -10,6 +12,7 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="w-[100vw] h-[100svh] bg-checker text-v-rl flex flex-col items-center overflow-hidden">
+        <Sprites />
         <Header />
         <Component />
       </body>
